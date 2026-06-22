ca58d80a93726b2b67a9df37a6cc240ffa65cf28
divyansh2901 <divyanshkashyap2901@gmail.com>
2026-06-23 01:02:29 +0530

    fixed dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,57 +2,51 @@ import { useEffect, useState } from "react";
 import api from "../api/axios";
 
 function Dashboard() {
-
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await api.get(
+          "/auth/me",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        setUser(res.data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     fetchUser();
   }, []);
 
-  const fetchUser = async () => {
-    try {
-
-      const token =
-        localStorage.getItem("token");
-
-      const res = await api.get(
-        "/auth/me",
-        {
-          headers: {
-            Authorization:
-              `Bearer ${token}`,
-          },
-        }
-      );
-
-      setUser(res.data);
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <div className="p-10">
-
       <h1 className="text-4xl font-bold">
         DevFlow Dashboard
       </h1>
 
-      {user && (
+      {user ? (
         <div className="mt-6">
-
           <h2 className="text-xl">
-            Welcome,
-            {" "}
-            {user.name}
+            Welcome, {user.name}
           </h2>
 
           <p>{user.email}</p>
-
         </div>
+      ) : (
+        <p className="mt-6">
+          Loading...
+        </p>
       )}
-
     </div>
   );
 }
